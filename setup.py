0843f66e2b0ddd29f199687f020cad9f1d5aefe0
안영진 <158057603+janyoungjin@users.noreply.github.com>
2024-05-06 16:58:21 +0900

    add long_description

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='SQLconn',
     version='0.0.1',
     description='This package facilitates easy SQL database integration.',
     author='janyoungjin',
     install_requires=['mysqlclient', 'pandas', 'cx_oracle', 'psycopg2', 'pymssql'],
+    long_description=long_description,
     packages=find_packages(exclude=[]),
     url='https://github.com/janyoungjin/SQLConn',
     keywords=['mysql', 'postgresql', 'sqlite', 'mssql', 'oracle', 'sql']
