8ae772e9a34f4911e63f4fa31aca150dea64ec1b
안영진 <158057603+janyoungjin@users.noreply.github.com>
2024-06-21 21:17:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='SQLConn',
-    version='0.0.15',
+    version='0.0.16',
     description='This package facilitates easy SQL database integration.',
     long_description=long_description,
     long_description_content_type="text/markdown",
